9bf0e60674a16d8478ac8ab5172a049287bdf0a5
Gutemberg Lamark <gtmbrglamark@gmail.com>
2022-11-11 14:16:25 -0300

    fix: ajustando caminho da pasta dist para deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -13,11 +13,11 @@ const AppDataSource = new DataSource({
 
   entities:
     process.env.NODE_ENV === "production"
-      ? ["dist/src/entities/*.js"]
+      ? ["dist/entities/*.js"]
       : ["src/entities/*.ts"],
   migrations:
     process.env.NODE_ENV === "production"
-      ? ["dist/src/migrations/*.js"]
+      ? ["dist/migrations/*.js"]
       : ["src/migrations/*.ts"],
 });
 
